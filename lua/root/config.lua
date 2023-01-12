458dff8f564842931319c618d1fb0deb94bc351e
David Kaméus <david.kameus@gmail.com>
2023-01-12 21:52:53 +0100

    fix shell on windows

--- lua/root/config.lua
+++ lua/root/config.lua
@@ -21,3 +21,20 @@ vim.opt.signcolumn = "yes"
 
 -- mark 80th char so I don't make too long lines                               
 vim.opt.colorcolumn = "80"
+
+--change default shell
+local function determine_shell()
+    -- use powershell by default
+    local shell = vim.opt.shell
+    local os = vim.loop.os_uname().sysname
+    if os == "Windows_NT" then
+        shell = "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
+        vim.opt.shellxquote = nil
+        -- vim.opt.shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command '
+
+    elseif os == "Linux" then
+        shell = "/usr/bin/bash"
+    end
+    return shell
+end
+vim.opt.shell = determine_shell()
