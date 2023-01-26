544b8ff369b38e371542daef680bca68a82a3224
David Kaméus <david.kameus@gmail.com>
2023-01-26 17:21:53 +0100

    remove all the treesitter parsers

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,10 +1,10 @@
 require'nvim-treesitter.configs'.setup {
     ensure_installed = {
         --"lua",
-        "rust",
-        "python",
-        "toml",
-        "help",
+        --"rust",
+        --"python",
+        --"toml",
+        --"help",
     },
 
     -- Install parsers synchronously (only applied to `ensure installed`)
